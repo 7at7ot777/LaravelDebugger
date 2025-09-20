8a5d048cea5ad2633a310e02eb3c850b15ff395f
Hathout <mohamed.hathout@seamlabs.com>
2025-09-20 04:52:14 +0300

    Caching, db and file storing, Use facade design pattern

--- routes/web.php
+++ routes/web.php
@@ -6,35 +6,26 @@ use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
-use function MongoDB\BSON\toJSON;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-//if(config('debugger.is_enabled')){
     Route::get(config('debugger.route_name'), \App\Livewire\DebugViewer::class)->name(config('debugger.route_name'));
-//}
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
-});
-
-require __DIR__.'/auth.php';
 
 
 
 
 Route::get('/test', function () {
 
+    Debugger::display(1);
+    Debugger::display(1.35);
+    Debugger::display(true);
+    Debugger::display(false);
+    Debugger::display('Hello, World!');
+    Debugger::display(\App\Models\Debug::get()->toArray());
 });
 
 
